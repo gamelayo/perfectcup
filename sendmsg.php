a60547c729d6d5bcffa71bd0f1f924768683073e
Gamel Ayodele <ayodelegamel@gmail.com>
2023-09-08 00:27:51 +0100

    new commit

---
+++ sendmsg.php
@@ -0,0 +1,88 @@
+<?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Open a new connection to the MySQL server
+$mysqli = new mysqli('localhost', 'root', '', 'perfectcup');
+
+//Output any connection error
+if ($mysqli->connect_error) {
+    die('Error : (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+}
+
+$fname = mysqli_real_escape_string($mysqli, $_POST['fname']);
+$email = mysqli_real_escape_string($mysqli, $_POST['email']);
+$message= mysqli_real_escape_string($mysqli, $_POST['message']);
+
+$email2 = "gamelbenayo@gmail.com";
+$subject = "Test Message";
+
+if (strlen($fname) > 50) {
+    echo 'fname_long';
+
+} elseif (strlen($fname) < 2) {
+    echo 'fname_short';
+
+} elseif (strlen($email) > 50) {
+    echo 'email_long';
+
+} elseif (strlen($email) < 2) {
+    echo 'email_short';
+
+} elseif (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+    echo 'eformat';
+
+} elseif (strlen($message) > 500) {
+    echo 'message_long';
+
+} elseif (strlen($message) < 3) {
+    echo 'message_short';
+
+} else {
+
+
+
+
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+                         //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'gamelbenayo@gmail.com';                     //SMTP username
+    $mail->Password   = 'Water@1234';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->AddReplyTo($email);
+    $mail->From = $email2;
+    $mail->FromName = $fname;
+    $mail->addAddress('gamelbenayo@gmail.com', 'Admin'); 
+    
+     
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+
+    $mail->Subject = $subject;
+    $mail->Body = $message;
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+}
